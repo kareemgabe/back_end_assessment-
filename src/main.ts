4b1d2b0e27c9a03c7d428f864b27dacc096ed2ef
kareem gaber <kgaber985@gmail.com>
2024-10-25 01:50:24 +0300

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
-  console.log('Application is running on: http://localhost:3001');
+  await app.listen(process.env.PORT ?? 3002);
+  console.log('Application is running on: http://localhost:3002');
 }
 bootstrap();
 
